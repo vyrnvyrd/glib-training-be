2e95f3c8fe53b68f4f871717df9952f1ecde93e0
Firna Firdiani <70745005+vyrnvyrd@users.noreply.github.com>
2022-07-27 16:42:20 +0700

    feature(book): add migration for table book

---
+++ Garuda.Modules.BookLibrary/Models/Contracts/IBookRepository.cs
@@ -0,0 +1,16 @@
+﻿// <copyright file="IBookRepository.cs" company="CV Garuda Infinity Kreasindo">
+// Copyright (c) CV Garuda Infinity Kreasindo. All rights reserved.
+// </copyright>
+
+using Garuda.Abstract.Contracts;
+using Garuda.Modules.BookLibrary.Models.Datas;
+
+namespace Garuda.Modules.BookLibrary.Models.Contracts
+{
+    /// <summary>
+    /// Entity Group Contract Repository
+    /// </summary>
+    public interface IBookRepository : IRepository
+    {
+    }
+}
