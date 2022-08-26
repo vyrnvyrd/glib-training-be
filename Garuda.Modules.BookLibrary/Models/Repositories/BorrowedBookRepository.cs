35f987a7cc8f8f67af1e6386aa97d7acac5f4991
Firna Firdiani <70745005+vyrnvyrd@users.noreply.github.com>
2022-08-26 13:23:46 +0700

    feature(borrow-book): simplify logic for increment

--- Garuda.Modules.BookLibrary/Models/Repositories/BorrowedBookRepository.cs
+++ Garuda.Modules.BookLibrary/Models/Repositories/BorrowedBookRepository.cs
@@ -30,10 +30,10 @@ namespace Garuda.Modules.BookLibrary.Models.Repositories
             }
 
             data.BorrowedDate = model.BorrowedDate;
-            data.BorrowedQuantity = model.BorrowedQuantity + data.BorrowedQuantity;
+            data.BorrowedQuantity += model.BorrowedQuantity;
             data.DueDate = model.DueDate;
             data.ReturnedDate = model.ReturnedDate;
-            data.ReturnedQuantity = model.ReturnedQuantity + data.ReturnedQuantity;
+            data.ReturnedQuantity =+ model.ReturnedQuantity;
             data.Remarks = model.Remarks;
             this.dbSet.Update(data);
         }
