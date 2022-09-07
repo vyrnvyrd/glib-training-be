0fe738359ef6e638c0328037e1dd69db5487058b
Firna Firdiani <70745005+vyrnvyrd@users.noreply.github.com>
2022-09-07 15:18:53 +0700

    feature(borrow-book): add api for get list overdue book

---
+++ Garuda.Modules.BookLibrary/Dtos/Responses/OverdueBookResponses.cs
@@ -0,0 +1,37 @@
+﻿using Sieve.Attributes;
+using System;
+
+namespace Garuda.Modules.BookLibrary.Dtos.Responses
+{
+    public class OverdueBookResponses
+    {
+        /// <summary>
+        /// Gets or sets a value indicating whether gets or sets for Book ID
+        /// </summary>
+        public Guid Id { get; set; }
+
+        /// <summary>
+        /// Gets or sets a value indicating whether gets or sets for Title
+        /// </summary>
+        [Sieve(CanFilter = true, CanSort = true)]
+        public string Title { get; set; }
+
+        /// <summary>
+        /// Gets or sets a value indicating whether gets or sets for Customer Name
+        /// </summary>
+        [Sieve(CanFilter = true, CanSort = true)]
+        public string CustomerName { get; set; }
+
+        /// <summary>
+        /// Gets or sets a value indicating whether gets or sets for Due Date
+        /// </summary>
+        [Sieve(CanFilter = true, CanSort = true)]
+        public DateTime DueDate { get; set; }
+
+        /// <summary>
+        /// Gets or sets a value indicating whether gets or sets for Duration
+        /// </summary>
+        [Sieve(CanFilter = true, CanSort = true)]
+        public double Duration { get; set; }
+    }
+}
--- Garuda.Modules.BookLibrary/Services/Contracts/IBorrowedBookServices.cs
+++ Garuda.Modules.BookLibrary/Services/Contracts/IBorrowedBookServices.cs
@@ -1,11 +1,20 @@
 ﻿using Garuda.Infrastructure.Dtos;
 using Garuda.Modules.BookLibrary.Dtos.Request;
+using Garuda.Modules.Common.Dtos.Responses;
+using Sieve.Models;
 using System.Threading.Tasks;
 
 namespace Garuda.Modules.BookLibrary.Services.Contracts
 {
     public interface IBorrowedBookServices
     {
+        /// <summary>
+        /// Get list of overdue book
+        /// </summary>
+        /// <param name="sieveModel"></param>
+        /// <returns>A <see cref="Task{TResult}"/> representing the result of the asynchronous operation.</returns>
+        Task<APIResponses> GetListOverdueBook(SieveModel sieveModel);
+
         /// <summary>
         /// Add or update borrow book
         /// </summary>
